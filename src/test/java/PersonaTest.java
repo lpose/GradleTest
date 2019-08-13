56f1422acc28e3d4d351b126fc0a011180dddac8
Lucas Pose <lucas.pose@mercadolibre.com>
2019-08-13 12:05:50 -0300

    Nuevo test sumarCincoALaEdad

--- src/test/java/PersonaTest.java
+++ src/test/java/PersonaTest.java
@@ -12,9 +12,16 @@ public class PersonaTest {
     }
 
     @Test
-    public void MultiplicarPorDosLaEdad(){
+    public void multiplicarPorDosLaEdad(){
         Persona personita = new Persona(40);
         personita.setEdad(personita.getEdad()*2);
         Assertions.assertThat(personita.getEdad()).isEqualTo(80);
+    }
+
+    @Test
+    public void sumarCincoALaEdad(){
+        Persona personita = new Persona(40);
+        personita.setEdad(personita.getEdad()+5);
+        Assertions.assertThat(personita.getEdad()).isEqualTo(46);
     }
 }
